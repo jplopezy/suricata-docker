e100ac672a1e76fdf15de41f9bce9d2b1d966b52
jplopezy <32151470@comunidadort.edu.ar>
2021-10-20 14:13:01 -0400

    suricata

--- ubuntu/Dockerfile/suricata-updatelog.sh
+++ ubuntu/Dockerfile/suricata-updatelog.sh
@@ -1,3 +1,4 @@
-sed -i "s/\\\r\\\n/\",\"/g" /var/log/suricata/eve.json
-sed -i 's/: /\":\"/g' /var/log/suricata/eve.json
-sed -i 's/,"",""//g' /var/log/suricata/eve.json
+cat /var/log/suricata/eve.json | grep "signature" >> /var/log/suricata/elk/eve2.json
+sed -i "s/\\\r\\\n/\",\"/g" /var/log/suricata/elk/eve2.json
+sed -i 's/: /\":\"/g' /var/log/suricata/elk/eve2.json
+sed -i 's/,"",""//g' /var/log/suricata/elk/eve2.json
