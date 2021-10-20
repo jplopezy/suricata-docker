2ebc16a10237967c9483b6c11e7948386087ead8
jplopezy <32151470@comunidadort.edu.ar>
2021-10-20 18:53:07 -0400

    suricata

--- ubuntu/Dockerfile/suricata-updatelog.sh
+++ ubuntu/Dockerfile/suricata-updatelog.sh
@@ -1,15 +1,2 @@
 
 
-cp /var/log/suricata/eve.json /var/log/suricata/eve2.json
-
-while true; do
-
-
-#echo "Logging 2..."  >> /var/log/suricata/eve.json
-#sed -i "s/\\\r\\\n/\",\"/g" /var/log/suricata/eve.json
-#sed -i 's/: /\":\"/g' /var/log/suricata/eve.json
-#sed -i 's/,"",""//g' /var/log/suricata/eve.json
-
- sleep 5;
-done
-
