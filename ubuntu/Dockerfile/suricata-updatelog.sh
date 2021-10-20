5f1d22a08ea61070c47ee946ed4232895e17f12b
jplopezy <32151470@comunidadort.edu.ar>
2021-10-20 16:48:42 -0400

    suricata

--- ubuntu/Dockerfile/suricata-updatelog.sh
+++ ubuntu/Dockerfile/suricata-updatelog.sh
@@ -1,16 +1,13 @@
 
-mkdir -p /var/log/suricata/elk/
-
-touch /var/log/suricata/elk/eve.json
-
 
 while true; do
 
-cat /var/log/suricata/eve.json | grep "signature" >> /var/log/suricata/elk/eve.json
-echo "Logging 2..."  >> /var/log/suricata/elk/eve.json
-sed -i "s/\\\r\\\n/\",\"/g" /var/log/suricata/elk/eve.json
-sed -i 's/: /\":\"/g' /var/log/suricata/elk/eve.json
-sed -i 's/,"",""//g' /var/log/suricata/elk/eve.json
+#cat /var/log/suricata/eve.json | grep "signature" >> /var/log/suricata/elk/eve.json
+
+echo "Logging 2..."  >> /var/log/suricata/eve.json
+sed -i "s/\\\r\\\n/\",\"/g" /var/log/suricata/eve.json
+sed -i 's/: /\":\"/g' /var/log/suricata/eve.json
+sed -i 's/,"",""//g' /var/log/suricata/eve.json
 
   sleep 5;
 done
