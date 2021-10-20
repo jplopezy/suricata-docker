3d17e463e797d405ed88e04215c94cd28d82bbb7
jplopezy <32151470@comunidadort.edu.ar>
2021-10-20 14:23:58 -0400

    suricata

--- ubuntu/Dockerfile/suricata-updatelog.sh
+++ ubuntu/Dockerfile/suricata-updatelog.sh
@@ -2,3 +2,4 @@ cat /var/log/suricata/eve.json | grep "signature" >> /var/log/suricata/elk/eve2.
 sed -i "s/\\\r\\\n/\",\"/g" /var/log/suricata/elk/eve2.json
 sed -i 's/: /\":\"/g' /var/log/suricata/elk/eve2.json
 sed -i 's/,"",""//g' /var/log/suricata/elk/eve2.json
+echo "llego aca"  >> /var/log/suricata/elk/eve2.json
