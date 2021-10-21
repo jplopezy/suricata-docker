edd4e398eb2268bfca4573b4dbd0c8d4cbbf143d
jplopezy <32151470@comunidadort.edu.ar>
2021-10-21 08:44:36 -0400

    suricata

--- ubuntu/Dockerfile/suricata-updatelog.sh
+++ ubuntu/Dockerfile/suricata-updatelog.sh
@@ -1,2 +1,9 @@
+while true; do
 
+cat /var/log/suricata/eve.json | grep "payload_printable" >  /var/log/suricata/eve2.json
+sed -i "s/\\\r\\\n/\",\"/g" /var/log/suricata/eve2.json
+sed -i 's/: /\":\"/g' /var/log/suricata/eve2.json
+sed -i 's/,"",""//g' /var/log/suricata/eve2.json
 
+ sleep 2;
+done
