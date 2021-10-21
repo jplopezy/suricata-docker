0080314837d672b9035509198c89e0bc615b6ecc
jplopezy <32151470@comunidadort.edu.ar>
2021-10-21 09:10:13 -0400

    suricata

--- ubuntu/Dockerfile/docker-entrypoint.sh
+++ ubuntu/Dockerfile/docker-entrypoint.sh
@@ -8,14 +8,12 @@ crontab -l > mycron
 
 echo "0 */6 * * * /etc/suricata/suricata-update.sh" >> mycron
 
-sh /etc/suricata/suricata-updatelog.sh
 
 crontab mycron
 
 rm mycron
 
  
-
 # Started suricata
 /usr/bin/suricata -c /etc/suricata/suricata.yaml -i eth0
 
